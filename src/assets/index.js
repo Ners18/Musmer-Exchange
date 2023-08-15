8aa3cbea267cef6a27690f2be0a4f7006ca45823
Ners18 <127780914+Ners18@users.noreply.github.com>
2023-08-15 14:16:57 +0300

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+import musmerlogo from "./musmerlogo.png";
+
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import Poundgif from "./tech/poundSpinning.gif";
+import dolargif from "./tech/dollarSpinning.gif";
+import dimension from "./tech/euroSpinning.gif";
+import threejs from "./tech/threejs.svg";
+
+
+import orangeup from "./tech/orangeup.png";
+import orangeupother from "./tech/orangeupother.png";
+
+import musmerpiccc from "./musmerpic3.jpg";
+import musmerpicc from "./musmerpic2.png";
+import musmerpic from "./musmer1.jpg";
+
+import dollar from "./tech/dollar.png"
+import Euro from "./tech/Euro.png";
+import yen from "./tech/yen.png";
+import Russian from "./tech/Russian.png";
+import Pound1 from "./tech/Pound1.png";
+import swiss from "./tech/swiss.png";
+import tl from "./tech/tl.png";
+import yuan from "./tech/yuan.png";
+import australian from "./tech/australian.png";
+import indian from "./tech/indian.png";
+import canadian from "./tech/canadian.png";
+import south from "./tech/south.png"
+export {
+  
+  logo,
+  musmerlogo,
+
+  github,
+  menu,
+  close,
+  threejs,
+ 
+  musmerpiccc,
+  musmerpicc,
+  musmerpic,
+
+  dollar,
+  Euro,
+  yen,
+  Russian,
+  Pound1,
+  swiss,
+  tl,
+  yuan,
+  australian,
+  indian,
+  canadian,
+  south,
+  dimension,
+  dolargif,
+  Poundgif,
+  orangeup,
+  orangeupother,
+};
