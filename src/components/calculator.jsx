64a69929e1a23d5b2f16b7190fcbab62daea834d
Ners18 <nersan.yahi@gmail.com>
2023-08-17 17:21:25 +0300

    border calculator

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -49,12 +49,15 @@ function Calculator() {
   }, [inputAmount, exchangeRate]);
 
   return (
-    <div className="None">
-      <div className="flex gap-4">
+    
+      <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div className="bg-gray-900 rounded-[20px] py-5 px-12 min-h-[180px] md:px-2 flex justify-evenly items-center flex-col">
+        <div className="w-full  p-[10px] rounded-[20px] ">
+      <div className="flex gap-4  rounded-[20px]">
         <select
           value={inputCurrency}
           onChange={(e) => setInputCurrency(e.target.value)}
-          className="font-bold text-1.5rem p-1 bg-white text-black rounded-md"
+          className="font-bold text-1.5rem p-1 bg-white  text-black rounded-md"
           name=""
           id="input_currency"
         >
@@ -72,13 +75,13 @@ function Calculator() {
           className="color-zinc-950 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
         />
       </div>
-      <div className="flex w-full text-center items-center justify-center p-5  ">
+      <div className="flex w-full text-center items-center  justify-center p-5  ">
         <button
           onClick={() => {
             setInputCurrency(outputCurrency);
             setOutputCurrency(inputCurrency);
           }}
-          className="flex items-center justify-center bg-slate-800 rounded-full p-2 w-[4rem] h-[4rem] text-[2rem] font-bold border-solid border-2 border-sky-500 active:translate-y-0 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 active:shadow-md active:translate-y-[-2px] p-4 "
+          className="flex items-center justify-center bg-slate-800 rounded-full p-2 w-[4rem] h-[4rem] text-[2rem]  font-bold border-solid border-2 border-sky-500 active:translate-y-0 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 active:shadow-md active:translate-y-[-2px] p-4 "
           id="exchange"
         >
           <HiMiniArrowPathRoundedSquare className="" />
@@ -110,7 +113,10 @@ function Calculator() {
         
         {`Changing from ${inputCurrency} to ${outputCurrency}`}
       </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ const styles = {
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-14",
     heroHeadText:
-      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+      "font-black text-white lg:text-[80px] sm:text-[10px] xs:text-[10px] text-[40px] lg:leading-[98px] mt-2",
     heroSubText:
       "text-[#DFD9FF] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
     sectionHeadText:
