d0a7261ec16f80fe7661e6d5dce8b4dbedc973c8
Ners18 <nersan.yahi@gmail.com>
2023-08-18 13:24:55 +0300

    color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Musmer Exchange",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "info@musmerexchange.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -47,7 +47,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Teşekkürler en kısa sürede size döneceğiz !");
 
           setForm({
             name: "",
@@ -59,7 +59,7 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert("Bişeyler hatalı gitdi, lütfen başdan deneyin !");
         }
       );
   };
--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,43 +1,45 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {HiMiniArrowPathRoundedSquare} from "react-icons/hi2";
+import { HiMiniArrowPathRoundedSquare } from "react-icons/hi2";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
-import  "../index.css";
+import "../index.css";
 import Calculator from "./calculator";
-const FeedbackCard = ({
-  index,
- 
-}) => (
-
+const FeedbackCard = ({ index }) => (
   <motion.div
-
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className=' xfqrt  p-10 rounded-3xl xs:w-[320px] w-full items-center justify-center '
-    
+    className="p-10 rounded-3xl flex-grow items-center justify-center "
   >
-      <Calculator />
-
+    <Calculator />
   </motion.div>
 );
 
 const Feedbacks = () => {
   return (
-    <div className={`mt-12 bg-black-100 rounded-[20px] items-center flex flex-col justify-center`}>
+    <div className=" green-pink-gradient p-[1px] rounded-[20px] shadow-card ">
       <div
-        className={`bg-black-100 rounded-2xl ${styles.padding} min-h-[300px] items-center`}
+        className={`mt-0.1 bg-black-100 rounded-[20px] items-center flex flex-col justify-center`}
       >
-        <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>Canlı </p>
-          <h2 className={styles.sectionHeadText}>Doviz kuru hesaplayıcı</h2>
-        </motion.div>
-      </div>
-      <div className={` mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7 items-center	`}>
-        {testimonials.map((testimonial, index) => (
-          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
-        ))}
+        <div
+          className={`bg-black-100 rounded-2xl ${styles.padding} min-h-[300px] items-center`}
+        >
+          <motion.div variants={textVariant()}>
+            <h2 className={styles.sectionHeadText}>Doviz kuru hesaplayıcı</h2>
+          </motion.div>
+        </div>
+        <div
+          className={` pb-14 ${styles.paddingX} flex flex-wrap gap-7 items-center	`}
+        >
+          {testimonials.map((testimonial, index) => (
+            <FeedbackCard
+              key={testimonial.name}
+              index={index}
+              {...testimonial}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
